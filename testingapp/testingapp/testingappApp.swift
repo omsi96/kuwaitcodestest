8c4b2cb95e8cf3eac80fc03c0eb32865ea770854
Omar <omsi96@users.noreply.github.com>
2020-12-28 18:28:18 +0300

    init commit

---
+++ testingapp/testingapp/testingappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingappApp.swift
+//  testingapp
+//
+//  Created by Omar Alibrahim on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct testingappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
